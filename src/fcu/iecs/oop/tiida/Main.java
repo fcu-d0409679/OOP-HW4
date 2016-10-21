fbc3dda2098b185a80802a1a62e3a90fc1df8b75
fcu-d0409679 <jimhou28@gmail.com>
2016-10-21 09:54:13 +0800

    執行印出輸入次數的正方形

---
+++ src/fcu/iecs/oop/tiida/Main.java
@@ -0,0 +1,39 @@
+package fcu.iecs.oop.tiida;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int loop=0;
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		NissanTiida nt = new NissanTiida();
+		
+		System.out.println("Please enter a number(number>0):");
+		
+		loop = scanner.nextInt();
+		
+		if(loop==0){
+			
+			System.out.println("輸入number不可為0");
+		}
+		
+		for(int i=0;i<loop;i++){
+			
+			nt.tiida();
+			
+		}
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
