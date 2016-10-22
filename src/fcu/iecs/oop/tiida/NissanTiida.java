fc6067f56b617198a3e5f7137e722d013d2bcb48
User-PC\User <jimhou28@gmail.com>
2016-10-22 21:35:58 +0800

    修改運算法，並使用 this 做呼叫此類別

--- src/fcu/iecs/oop/tiida/NissanTiida.java
+++ src/fcu/iecs/oop/tiida/NissanTiida.java
@@ -2,38 +2,24 @@ package fcu.iecs.oop.tiida;
 
 public class NissanTiida {
 
-	int count=0;
+	private int count=0;//型態為 private 表私有的 只有此類別成員可用
 	
 	public void tiida(){
 		
-		if(count==0){
-			
-			System.out.println("*");			
-			count++;
-			System.out.println();
-		}
+		this.count++;
 		
-		
-		else{
-			
-			for(int i=0;i<=count;i++){
+		for(int i=0;i<this.count;i++){ 
+			for(int j=0;j<this.count;j++){ 
 				
-				for(int j=0;j<=count;j++){
-					
-					System.out.print("*");
-				}
-				System.out.println();
-			}
+				System.out.print("*"); 
+			} 
 			
-			
-		}
+			System.out.println(); 
+		} 
 		
-		
-		
-		
-		
-	}
-	
-	
-	
+		System.out.print("\n\n"); 
+	} 
+
 }
+	
+
